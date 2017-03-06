1dc465738dbef78d13abe9c14766df300fdc65c4
Kevin Hawkins <khawkins@netradius.com>
2017-03-06 14:20:09 -0700

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {MessagingService} from "./stompjs/messaging.service";
+import {isUndefined} from "util";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  providers: [MessagingService],
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+
+  constructor(private messagingService: MessagingService) { }
+
+  connectMessaging(): void {
+    this.messagingService.connectMessaging("//localhost:8080/connect");
+    this.messagingService.getStatusObservable().subscribe(status => {
+      if (this.username === "") {
+        this.username = "Guest"
+      }
+      this.messages += status.command + " as " + this.username + "\n";
+    });
+    this.messagingService.getObservable().subscribe(payload => {
+      this.message = "";
+      this.messages += payload.username + " (" + this.formatDateTime(new Date(payload.dateTime))  + "): " + payload.message + "\n";
+    });
+  }
+
+  sendMessage():void {
+    this.messagingService.sendMessage(this.username, this.message);
+  }
+
+  private formatDateTime(date: Date):string {
+    return date.toLocaleTimeString().replace(/([\d]+:[\d]{2})(:[\d]{2})(.*)/, "$1$3");
+  }
+
+  title = 'WebSocket demo: A simple messaging app';
+  username:string = "";
+  message: string = "";
+  messages:string = "";
 }
---
+++ src/app/stompjs/messaging.service.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@angular/core';
+
+import {Http, Response} from "@angular/http";
+import {Observable, Subject} from "rxjs";
+
+import 'stompjs';
+
+
+declare let Stomp:any;
+var SockJS = require('sockjs-client');
+
+@Injectable()
+export class MessagingService {
+
+
+  private stompClient;
+  private stompMessage: Subject<any> = new Subject<any>();
+  private stompConnectionStatus: Subject<any> = new Subject<any>();
+
+  constructor(private http: Http) { }
+
+  public connectMessaging(url: string) : any {
+    let self = this;
+    let webSocket = new SockJS(url);
+    //let webSocket = new WebSocket(url);
+    this.stompClient = Stomp.over(webSocket);
+    this.stompClient.debug = null;
+    this.stompClient.connect({}, function (frame) {
+      self.stompConnectionStatus.next(frame);
+      self.stompClient.subscribe('/topic/messages', function (res) {
+          self.stompMessage.next(JSON.parse(res.body));
+      });
+      return frame;
+    });
+  }
+
+  public sendMessage(username: string, message: string) {
+    this.stompClient.send("/add", {}, JSON.stringify({"username": username, "message": message}));
+  }
+
+  public getObservable(): Observable<any> {
+    // return this.stompClient.asObservable();
+    return this.stompMessage;
+  }
+
+  public getStatusObservable(): Observable<any> {
+    return this.stompConnectionStatus;
+  }
+
+}
